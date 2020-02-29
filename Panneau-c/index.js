91c8f92f236c888489d31e8c78b33662984fca88
adrianutge <adrinutge@gmail.com>
2020-02-29 18:14:15 +0100

    Ajout de JSON

--- Panneau-c/index.js
+++ Panneau-c/index.js
@@ -1,1 +1,47 @@
+// Declarations des varibles
+
+// Exemple de menu aui sera plus tard directement fait depuis le panneau d'administartion
+
+let menu =
+{
+    "lundi": {
+        "entre": "Fromage",
+        "plat": "Pates",
+        "dessert": "Mousse au chocolat",
+    },
+    "mardi": {
+        "entre": "Salade",
+        "plat": "Fondue",
+        "dessert": "Gateau au chocolat",
+    },
+    "jeudi": {
+        "entre": "Pate de campage",
+        "plat": "Quiche lorraine",
+        "dessert": "mousse au chocolat",
+    },
+    "vendredi": {
+        "entre": "Taboule",
+        "plat": "Steak tartare",
+        "dessert": "mousse au chocolat",
+    },
+}
+
+// Entrées
+let entrelundi = menu.lundi.entre;
+let entremardi = menu.mardi.entre;
+let entrejeudi = menu.jeudi.entre;
+let entrevendredi = menu.vendredi.entre;
+
+// Plats 
+let platlundi = menu.lundi.plat;
+let platmardi = menu.mardi.plat;
+let platjeudi = menu.jeudi.plat;
+let platvendredi = menu.vendredi.plat;
+
+// Dessert
+
+let dessertlundi = menu.lundi.dessert;
+let dessertmardi = menu.mardi.dessert;
+let dessertjeudi = menu.jeudi.dessert;
+let dessertvendredi = menu.vendredi.dessert;
 
