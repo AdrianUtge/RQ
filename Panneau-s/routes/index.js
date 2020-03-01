2a2cfc479d2a30cd32d6f72c04e74a8d6feec152
adrianutge <adrinutge@gmail.com>
2020-03-01 14:21:13 +0100

    landige page full route

--- Panneau-s/routes/index.js
+++ Panneau-s/routes/index.js
@@ -4,5 +4,9 @@ var router = express.Router();
 let landing = require('../controllers/landing.js')
 /* GET home page. */
 router.get('/', landing.get_landing);
+router.post('/', landing.submit_lead);
+
+
+
 
 module.exports = router;
