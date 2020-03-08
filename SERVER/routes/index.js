f132418200d5d35a3fa7a9d6a57b1c7e504572f7
adrianutge <adrinutge@gmail.com>
2020-03-08 13:04:41 +0100

    Abs route ajoute

--- SERVER/routes/index.js
+++ SERVER/routes/index.js
@@ -5,13 +5,16 @@ const router = express.Router();
 const admin = require('./admin');
 const main = require('./main');
 const menu = require('./menu');
+const abs = require('./abs')
 
-router.get('/admin', admin);
 
 router.get('/', main);
 
-router.get('/admin/menu', menu);
 
+//Admin Panel
+router.get('/admin', admin);
+router.get('/admin/menu', menu);
+router.get('/admin/abs', abs)
 
 
 module.exports = router;
