8d94fa7ee2d2a3c2203867d541b456bac389249f
AdrianUtge <49118376+AdrianUtge@users.noreply.github.com>
2020-04-18 20:19:02 +0200

    Update db_main.js

--- SERVER/middleware/db_main.js
+++ SERVER/middleware/db_main.js
@@ -6,7 +6,7 @@ moment.locale('fr');
 
 
 // // Connection URL
-const url = 'mongodb+srv://Admin:birsW1sW3IP8nhh8@rq-s-zoen2.mongodb.net/test?retryWrites=true&w=majority';
+const url = '';
 
 // // Database Name
 const dbName = 'Prof';
